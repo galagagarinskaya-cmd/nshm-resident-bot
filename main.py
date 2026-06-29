5489666555ceac902b6670e5d4322ecae106dcc5
Gala <gala.gagarinskaya@gmail.com>
2026-06-30 01:25:36 +0300

    Fix ChatPermissions parameter names (remove can_send_media_messages)

--- main.py
+++ main.py
@@ -113,7 +113,6 @@ async def handle_new_chat_members(update: Update, context: ContextTypes.DEFAULT_
                 user_id=user_id,
                 permissions=ChatPermissions(
                     can_send_messages=False,
-                    can_send_media_messages=False,
                     can_send_other_messages=False,
                     can_add_web_page_previews=False,
                     can_send_polls=False,
